d6c5860155088a24b1aff9f92a1ee3005a207f9a
hopvinna <41123465+hopvinna@users.noreply.github.com>
2021-02-08 19:00:22 +0530

    updated js file

--- Dicee/index.js
+++ Dicee/index.js
@@ -1,20 +1,16 @@
-// alert("warning");
-
 var randomNumber1 = Math.ceil(Math.random() * 6);
-var image1 = document.querySelector(".img1");
-image1.setAttribute("src", "images/dice"+ randomNumber1 +".png");
+document.querySelector(".img1").setAttribute("src","images/dice"+ randomNumber1 +".png");
 
 var randomNumber2 = Math.ceil(Math.random() * 6);
-var image2 = document.querySelector(".img2");
-image2.setAttribute("src", "images/dice"+ randomNumber2 +".png");
+document.querySelector(".img2").setAttribute("src","images/dice"+ randomNumber2 +".png");
 
 
 if(randomNumber1 > randomNumber2){
   document.querySelector("h1").innerHTML = "<i class='fas fa-flag'></i> Player 1 Wins!";
 }
 else if(randomNumber1 === randomNumber2){
-  document.querySelector("h1").innerHTML = " Player 2 Wins!<i class='fas fa-flag'></i>";
+  document.querySelector("h1").innerHTML = "Draw!";
 }
 else{
-  document.querySelector("h1").innerHTML = " Draw!";
+  document.querySelector("h1").innerHTML = "Player 2 Wins!<i class='fas fa-flag'></i>";
 }
